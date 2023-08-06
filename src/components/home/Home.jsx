359a1312958af5698fe7db88effc00444760eb60
Chris Mbah <mbahchris46@gmail.com>
2023-08-06 21:56:48 +0100

    Featured cards were added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './hero/Hero'
+import Featured from './Featured/Featured'
 
 export default function Home() {
   return (
     <>
     <Hero />
+    <Featured />
     </>
   )
 }

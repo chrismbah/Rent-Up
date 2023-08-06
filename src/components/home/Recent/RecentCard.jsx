41ac7ca8ff6e2d3e67d18062d75507abbae6f7d7
Chris Mbah <mbahchris46@gmail.com>
2023-08-06 23:00:49 +0100

    RecentCard component styling

--- src/components/home/Recent/RecentCard.jsx
+++ src/components/home/Recent/RecentCard.jsx
@@ -5,12 +5,30 @@ export default function RecentCard() {
     <>
       <div className="content grid3 mtop">
         {list.map((item, index) => {
-          const { cover, name, location, category, price, type } = val;
-          return <div className="box shadow" key={index}>
-            <div className="img">
+          const { cover, name, location, category, price, type } = item;
+          return (
+            <div className="box shadow" key={index}>
+              <div className="img">
                 <img src={cover} alt="cover" />
+              </div>
+              <div className="text">
+                <div className="category flex">
+                  <span
+                    style={{
+                      background:
+                        category === "For Sale" ? "#25b5791a" : "#ff98001a",
+                      color: category === "For Sale" ? "#25b579" : "#ff9800"
+                    }}
+                  >
+                    {category}
+                  </span>
+                  <i className="fa fa-heart"></i>
+                </div>
+                <h4>{name}</h4>
+                <p><i className="fa fa-location-dot"></i>{location}</p>
+              </div>
             </div>
-          </div>;
+          );
         })}
       </div>
     </>

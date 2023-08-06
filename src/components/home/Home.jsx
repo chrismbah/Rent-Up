94745db6889c210f3b44a1e8db78a07c98b8a898
Chris Mbah <mbahchris46@gmail.com>
2023-08-06 22:32:06 +0100

    Added Recent and Recent card component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import Hero from './hero/Hero'
+import Hero from './Hero/Hero'
 import Featured from './Featured/Featured'
+import Recent from './Recent/Recent'
 
 export default function Home() {
   return (
     <>
     <Hero />
     <Featured />
+    <Recent />
     </>
   )
 }

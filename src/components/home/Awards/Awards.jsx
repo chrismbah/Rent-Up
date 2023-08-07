f0106e6544b815891972b3c84e68fe5f8b95d38c
Chris Mbah <mbahchris46@gmail.com>
2023-08-07 08:56:11 +0100

    Implemented Awards category

--- src/components/home/Awards/Awards.jsx
+++ src/components/home/Awards/Awards.jsx
@@ -12,10 +12,16 @@ export default function Awards() {
             title="Over 1,240,000+ Happy Users Being with us still they love our services"
             subtitle="Our Awards"
           />
-          <div className="content grid5 mtop">
-            {awards.map((awards,index)=>{
-                <div className="box" key={index}></div>
-            })}
+          <div className="content grid4 mtop">
+            {awards.map((award,index)=>(
+                <div className="box" key={index}>
+                    <div className="icon">
+                        <span>{award.icon}</span>
+                    </div>
+                    <h1>{award.num}</h1>
+                    <p>{award.name}</p>
+                </div>
+            ))}
           </div>
         </div>
       </section>

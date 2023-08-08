8b5c768d332906dbc0eca76de1f0fb3525c24013
Chris Mbah <mbahchris46@gmail.com>
2023-08-08 02:06:48 +0100

    Footer styling

---
+++ src/components/common/footer/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Footer.css";
+import { footer } from "../../data/Data";
+
+export default function Footer() {
+  return (
+    <>
+      <section className="footerContact">
+        <div className="container">
+          <div className="send flex">
+            <div className="text">
+              <h1>Do You Have Questions</h1>
+              <p>We'll help you to grow your career and growth.</p>
+            </div>
+            <button className="btn5">Contact Us Today</button>
+          </div>
+        </div>
+      </section>
+      <footer>
+        <div className="container">
+          <div className="box">
+            <div className="logo">
+              <img src="../images/logo-light.png" alt="" />
+              <h2>Do You Need Help With Anything</h2>
+              <p>
+                Recieve updates, hot deals,tutorials ,discounts sent straight in
+                your inbox every month
+              </p>
+              <div className="input flex">
+                <input type="text" placeholder="Email Address" />
+                <button>Subscribe</button>
+              </div>
+            </div>
+          </div>
+          {footer.map((val) => {
+            return (
+              <div className="box">
+                <h3>{val.title}</h3>
+                <ul>
+                  {val.text.map((item) => {
+                    const { list } = item;
+                    return <li>{list}</li>;
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      </footer>
+      <div className="legal">
+        <span>© 2023 RentUP. Designed By Chris Mbah</span>
+      </div>
+    </>
+  );
+}
--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../common/header/Header";
 import Home from "../home/Home";
+import Footer from "../common/footer/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import { createContext } from "react";
@@ -20,6 +21,7 @@ export default function Pages() {
               element={<Home setlistNum={setListNum} listNum={listNum} />}
             />
           </Routes>
+          <Footer />
         </Router>
       </AppContext.Provider>
     </>

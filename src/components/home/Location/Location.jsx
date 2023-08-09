9a9660378980a0da9ea613548b11a01ec71a7476
Chris Mbah <mbahchris46@gmail.com>
2023-08-09 20:34:26 +0100

    Improved Location component responsiveness

--- src/components/home/Location/Location.jsx
+++ src/components/home/Location/Location.jsx
@@ -13,18 +13,18 @@ export default function Location() {
             subtitle="Explore various properties you wish to own or rent by loaction including various famous tourist sites"
           />
           <div className="content grid3 mtop">
-            {locations.map((location,index)=>(
-                <div className="box" key={index}>
-                    <img src={location.cover} alt="location" />
-                    <div className="overlay">
-                      <h5>{location.name}</h5>
-                      <p>
-                        <label >{location.Villas}</label>
-                        <label >{location.Offices}</label>
-                        <label >{location.Apartments}</label>
-                      </p>
-                    </div>
+            {locations.map((location, index) => (
+              <div className="box" key={index}>
+                <img src={location.cover} alt="location" />
+                <div className="overlay">
+                  <h5>{location.name}</h5>
+                  <p>
+                    <label>{location.Villas}</label>
+                    <label>{location.Offices}</label>
+                    <label>{location.Apartments}</label>
+                  </p>
                 </div>
+              </div>
             ))}
           </div>
         </div>

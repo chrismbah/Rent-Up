ee68b784eb590c92f0ccc3010e6b8a97dcdff93d
Chris Mbah <mbahchris46@gmail.com>
2023-10-31 09:20:12 +0100

    Trying to fix  build bug

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./header.css";
+import "./Header.css";
 import { Link } from "react-router-dom";
 import { nav } from "../../data/Data";
 import { useContext } from "react";
